0eecb543cdea3cda42ddfb178885235e3fe2efa9
Marcelo Rovai <Mjrovai@gmail.com>
2026-01-21 14:34:29 -0300

    Add files via upload

--- IMG_CLASS/python_scripts/setup_test.py
+++ IMG_CLASS/python_scripts/setup_test.py
@@ -1,12 +1,12 @@
-import tflite_runtime.interpreter as tflite
+from ai_edge_litert.interpreter import Interpreter
 import numpy as np
 from PIL import Image
 
 print("NumPy:", np.__version__)
 print("Pillow:", Image.__version__)
 
-# Try to create a TFLite Interpreter
+# Try to create a LiteRT Interpreter
 model_path = "./models/mobilenet_v2_1.0_224_quant.tflite"
-interpreter = tflite.Interpreter(model_path=model_path)
+interpreter = Interpreter(model_path=model_path)
 interpreter.allocate_tensors()
-print("TFLite Interpreter created successfully!")
+print("LiteRT Interpreter created successfully!")
